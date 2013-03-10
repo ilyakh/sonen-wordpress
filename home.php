e17fe9538746e334998eee6cb0e1219d6c191367
unknown <ilyakh@ulrik.uio.no>
2013-03-10 04:16:58 +0100

    wraps up the branch

--- home.php
+++ home.php
@@ -41,10 +41,13 @@ class Layout {
             $n = 1;
         }
 
-        $this->rows[] = new Row( $this->gridSize / $n );
+        $row = new Row( $this->gridSize / $n );
+        $this->rows[] = $row;
+
+        return $row;
     }
 
-    function addElement( $element ) {
+    function addElement( $element="" ) {
 
         if ( $this->getCurrentRow() != null ) {
 
@@ -56,7 +59,6 @@ class Layout {
             if ( $this->getCurrentRow() != null ) {
                 $this->getCurrentRow()->addElement( $element );
             }
-
         }
     }
 
@@ -64,6 +66,19 @@ class Layout {
         return $this->rows[$this->currentRow];
     }
 
+
+    function toArray() {
+        $rows = array('<div class="site">');
+
+        foreach ( $this->rows as $row ) {
+            $rows = array_merge( $rows, $row->toArray() );
+        }
+
+        $rows[] = "</div>";
+
+        return $rows;
+
+    }
 
     // -----------------------------------------------------------------------------------------------------------------
     function __toString() {
@@ -82,7 +97,7 @@ class Layout {
 class Row {
     protected $elements;
 
-    // faller tilbake til elementstørrelse '4', hvis ikke oppgitt
+    // falls back to default element size=4, yields three elements per row
     function __construct( $elementSize=4, $gridSize=12 ) {
         $this->elements = array();
         $this->gridSize = $gridSize;
@@ -100,6 +115,7 @@ class Row {
         if ( $this->isFull() ) {
             return false;
         } else {
+            $element->setSize( $this->elementSize );
             $this->elements[] = $element;
             return true;
         }
@@ -113,66 +129,93 @@ class Row {
         return ( count( $this->elements ) == 0 );
     }
 
+    function populate() {
+        for ( $i = 0; $i < ( $this->gridSize / $this->elementSize ); $i++ ) {
+            $element = new Element();
+            $element->setSize( $this->elementSize );
+            $this->elements[] = $element;
+        }
+    }
+
+    function toArray() {
+
+        $elements = array('<div class="row-fluid preview-row">');
+
+        foreach ( $this->elements as $element ) {
+            $elements = array_merge( $elements, $element->toArray() );
+        }
+
+        $elements[] = '</div>';
+
+        return $elements;
+    }
+
     // -----------------------------------------------------------------------------------------------------------------
     function __toString() {
         $elements = array();
-        echo '<h1>'.count( $this->elements ).'</h1>';
         $template = '<div class="span%s">%s</div>';
 
         foreach ( $this->elements as $element ) {
             $elements[] = sprintf( $template, $this->elementSize, $element );
         }
 
-        return implode( "\n", $elements );
+        return '<h1>'. count( $this->elements ).'</h1>' . implode( "\n", $elements );
     }
 }
 
 class Element {
 
     protected $content;
+    protected $size;
 
-    function __construct( $content ) {
+    function __construct( $content="" ) {
         $this->content = $content;
+        $this->size = 4;
+    }
+
+    function toArray() {
+
+        return array(
+            sprintf( '<div class="span%s preview">', $this->size ),
+            'content',
+            '</div>'
+        );
+    }
+
+    function setSize( $size ) {
+        $this->size = $size;
     }
 
     // -----------------------------------------------------------------------------------------------------------------
     function __toString() {
-        return sprintf( "Element: %s", $this->content );
+        return sprintf( "Element" );
     }
 
 }
 
 
-$layout = new Layout();
-$layout->addRow(2);
-$layout->addRow(3);
-$layout->addRow(3);
-$layout->addRow(3);
-$layout->addRow(0);
+if ( have_posts() )  {
 
-$layout->addElement( new Element( "aaaa" ) );
-$layout->addElement( new Element( "aaaa" ) );
-$layout->addElement( new Element( "aaaa" ) );
-$layout->addElement( new Element( "aaaa" ) );
-$layout->addElement( new Element( "aaaa" ) );
-$layout->addElement( new Element( "aaaa" ) );
-$layout->addElement( new Element( "aaaa" ) );
-$layout->addElement( new Element( "aaaa" ) );
-$layout->addElement( new Element( "aaaa" ) );
-$layout->addElement( new Element( "aaaa" ) );
-$layout->addElement( new Element( "aaaa" ) );
-$layout->addElement( new Element( "aaaa" ) );
-$layout->addElement( new Element( "aaaa" ) );
-$layout->addElement( new Element( "aaaa" ) );
-$layout->addElement( new Element( "aaaa" ) );
-$layout->addElement( new Element( "aaaa" ) );
-$layout->addElement( new Element( "aaaa" ) );
-$layout->addElement( new Element( "aaaa" ) );
-$layout->addElement( new Element( "aaaa" ) );
-$layout->addElement( new Element( "aaaa" ) );
+    $layout = new Layout();
+
+    $post_count = $wp_query->post_count;
+
+    $layout->addRow(3)->populate();
 
 
-echo $layout;
+    $recipe = $layout->toArray();
+
+    foreach ( $recipe as $element ) {
+        if ( $element == "content" ) {
+            the_post();
+            get_template_part( 'content', get_post_format() );
+        } else {
+            echo $element;
+        }
+    }
+
+
+}
 
 
 
@@ -181,7 +224,12 @@ echo $layout;
 // ?>
 
 
+
+
+
 <div class="site">
+
+
 
 <?php if ( have_posts() ) : /* Starts the LOOP */ ?>
 
@@ -342,11 +390,14 @@ echo $layout;
 
     <?php endif; ?>
 
+</div>
 
 <?php twentytwelve_content_nav( 'nav-below' ); ?>
 
-</div>
 
+
+
+</div>
 <?php get_sidebar( 'blogroll' ); ?>
 
 
