3edb3817bd39a81a3e83642cf2a75960d0bf6c0d
unknown <ilyakh@ulrik.uio.no>
2013-03-18 12:25:21 +0100

    fixes the page display troubles; by rewriting content-page.php

---
+++ content-page.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage sonen
+ * @version 3.0
+ */
+?>
+
+<div class="page-container">
+    <div class="row-fluid page-row">
+        <div class="span12">
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+            <div class="entry-content"><?php the_content(); ?></div>
+        </div>
+    </div>
+</div><!-- #post -->
