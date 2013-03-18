02e9304052ba8c91b66504fdaa6d4538d0cace03
unknown <ilyakh@ulrik.uio.no>
2013-03-18 12:20:01 +0100

    fixes the page display troubles; by rewriting content-page.php

--- content.php
+++ content.php
@@ -1,104 +1,97 @@
 <?php
 /**
- * The default template for displaying content. Used for both single and index/archive/search.
- *
  * @package WordPress
  * @subpackage sonen
  */
 ?>
 
 <?php if ( ! is_single() || is_archive() ) : /* PREVIEW */ ?>
+
     <div class="preview-container">
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-        <header class="entry-header">
+            <header class="entry-header">
 
-        <div class="foundation">
-            <div class="entry-thumbnail">
-                <a href="<?php the_permalink(); ?>">
-                    <?php the_post_thumbnail('thumbnail'); ?>
-                </a>
-            </div>
-        </div>
+                <div class="foundation">
+                    <div class="entry-thumbnail">
+                        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
+                    </div>
+                </div>
 
-        <h1 class="entry-title">
-            <a href="<?php the_permalink(); ?>">
-                <?php the_title(); ?>
-            </a>
-        </h1>
+                <h1 class="entry-title">
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_title(); ?>
+                    </a>
+                </h1>
+            </header>
 
-        </header>
+            <?php if ( is_search() ) : // Only display Excerpts for Search ?>
 
-
-
-        <?php if ( is_search() ) : // Only display Excerpts for Search ?>
-
-            <div class="entry-summary">
-                <?php the_excerpt(); ?>
-            </div>
-
-        <?php else : ?>
-
-            <div class="entry-content">
-                <a href="<?php the_permalink(); ?>"
-                    title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark">
+                <div class="entry-summary">
                     <?php the_excerpt(); ?>
-                </a>
-            </div>
+                </div>
 
-        <?php endif; ?>
+            <?php else : ?>
+
+                <div class="entry-content">
+                    <a href="<?php the_permalink(); ?>"
+                       title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark">
+                        <?php the_excerpt(); ?>
+                    </a>
+                </div>
+
+            <?php endif; ?>
         </article>
-
     </div>
 
 
-    <?php else : /* SINGLE */ ?>
-    <div class="single-container">
+<?php else : /* SINGLE */ ?>
+<div class="single-container">
 
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-            <div class="row-fluid hidden-phone">
-                <div class="span12 entry-title-container">
-                    <header class="entry-header">
+        <div class="row-fluid hidden-phone">
+            <div class="span12 entry-title-container">
+                <header class="entry-header">
 
-                        <div class="">
-                            <div class="entry-top">
-                                <div class="entry-top-right"><?php the_date(); ?></div>
-                                <div class="entry-top-left">&nbsp;</div>
-                            </div>
-                            <div class="foundation">
-                                <div class="entry-thumbnail">
-                                    <?php the_post_thumbnail('large'); ?>
-                                </div>
-                                <div class="title-overlay">
-                                    <h1><?php the_title(); ?></h1>
-                                </div>
-                            </div>
+                    <div class="">
+                        <div class="entry-top">
+                            <div class="entry-top-right"><?php the_date(); ?></div>
+                            <div class="entry-top-left">&nbsp;</div>
                         </div>
-                    </header>
-                </div>
-            </div>
-
-            <div class="row-fluid">
-                <div class="span9">
-                    <div class="entry-content" style="background-color: white;">
-
-                        <div class="visible-phone">
-                            <div>
+                        <div class="foundation">
+                            <div class="entry-thumbnail">
                                 <?php the_post_thumbnail('large'); ?>
                             </div>
-                            <h1 class="entry-mobile-title visible-phone"><?php the_title(); ?></h1>
+                            <div class="title-overlay">
+                                <h1><?php the_title(); ?></h1>
+                            </div>
                         </div>
-
-                        <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?>
-                        <?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
                     </div>
-                </div>
-                <div class="span3">
-                    <?php get_coauthor_list_sidebar() ?>
+                </header>
+            </div>
+        </div>
+
+        <div class="row-fluid">
+            <div class="span9">
+                <div class="entry-content" style="background-color: white;">
+
+                    <div class="visible-phone">
+                        <div>
+                            <?php the_post_thumbnail('large'); ?>
+                        </div>
+                        <h1 class="entry-mobile-title visible-phone"><?php the_title(); ?></h1>
+                    </div>
+
+                    <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?>
+                    <?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
                 </div>
             </div>
-        </article>
+            <div class="span3">
+                <?php get_coauthor_list_sidebar() ?>
+            </div>
+        </div>
+    </article>
 
 
     </div>
--- page.php
+++ page.php
@@ -1,13 +1,10 @@
 <?php get_header(); ?>
 
-<div id="page">
+<div id="page" class="page-container">
+<?php while ( have_posts() ) : the_post(); ?>
 
-<div class="row-fluid page-row">
-    <?php while ( have_posts() ) : the_post(); ?>
-    <div class="span8">
-        <?php get_template_part( 'content', 'page' ); ?>
-    </div>
-</div>
+<?php get_template_part( 'content', 'page' ); ?>
+
 <div class="row-fluid page-row">
     <div class="span8">
         <?php comments_template( '', true ); ?>
@@ -15,48 +12,50 @@
     <?php endwhile; ?>
 </div>
 
-</div>
 
 <div class="row-fluid preview-row">
 
-<?php
-$temp_query = clone $wp_query;
+    <?php
+    $temp_query = clone $wp_query;
 
-query_posts(
-    array(
-        'posts_per_page' => 3,
-        'orderby' => 'rand',
-        'post__not_in' => array( get_the_ID() )
-    )
-);
+    query_posts(
+        array(
+            'posts_per_page' => 3,
+            'orderby' => 'rand',
+            'post__not_in' => array( get_the_ID() )
+        )
+    );
 
-?>
+    ?>
 
-<?php while ( have_posts()) : the_post(); ?>
-    <div class="span4 preview">
-        <div class="preview-container">
-            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <header class="entry-header">
-                    <div class="foundation">
-                        <div class="entry-thumbnail">
-                            <a href="<?php the_permalink(); ?>">
-                                <?php the_post_thumbnail('thumbnail'); ?>
-                            </a>
+    <?php while ( have_posts()) : the_post(); ?>
+        <div class="span4 preview">
+            <div class="preview-container">
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <header class="entry-header">
+                        <div class="foundation">
+                            <div class="entry-thumbnail">
+                                <a href="<?php the_permalink(); ?>">
+                                    <?php the_post_thumbnail('thumbnail'); ?>
+                                </a>
+                            </div>
                         </div>
-                    </div>
 
-                    <h1 class="entry-title">
-                        <a href="<?php the_permalink(); ?>">
-                            <?php the_title(); ?>
-                        </a>
-                    </h1>
+                        <h1 class="entry-title">
+                            <a href="<?php the_permalink(); ?>">
+                                <?php the_title(); ?>
+                            </a>
+                        </h1>
 
-                </header><!-- .entry-header -->
-            </article>
+                    </header><!-- .entry-header -->
+                </article>
+            </div>
         </div>
-    </div>
-<?php endwhile; ?>
-<?php $wp_query = clone $temp_query; ?>
+    <?php endwhile; ?>
+    <?php $wp_query = clone $temp_query; ?>
+
+</div>
+
 </div>
 
 <?php get_footer(); ?>
